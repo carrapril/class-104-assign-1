39bbbdd18b4af708e8757fb59b61bd3132afab6f
carrapril <97204002+carrapril@users.noreply.github.com>
2022-02-09 19:45:30 -0500

    Add files via upload

---
+++ about.js
@@ -0,0 +1,29 @@
+let pet1 ={
+    name:"Scooby",
+    petAge:50,
+    petGender:"Male"
+}
+console.log(pet1);
+
+let pet2 ={
+    name:"Daisy",
+    petAge:3,
+    petGender:"Female"
+}
+console.log(pet2);
+let pet3 ={
+    name:"Sunny",
+    petAge:5,
+    petGender:"Male"
+}
+console.log(pet3);
+let pet4 ={
+    name:"Janis",
+    petAge:21,
+    petGender:"Female"
+}
+console.log(pet4);
+
+alert("You have 4 Registered Pets!");
+
+
